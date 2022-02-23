ed3f42e9d9caef2b535ce45e3bd28be1627c626f
alex rodriguez <alex@horchatadesign.com>
2022-02-22 22:07:49 -0600

    Create faqs.js

---
+++ src/faqs.js
@@ -0,0 +1,29 @@
+export const faqs = [
+  {
+    id: 'tab-one',
+    title: 'Perferendis expedita',
+    content:
+      '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi odit ea recusandae expedita tempore voluptas voluptatem consectetur iusto quae, consequuntur minima hic corrupti pariatur facilis porro fugit vero? Perferendis, expedita.</p>',
+  },
+  {
+    id: 'tab-two',
+    title: 'Modi odit ea recusandae expedita',
+    content:
+      '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi odit ea recusandae expedita tempore voluptas voluptatem consectetur iusto quae, consequuntur minima hic corrupti pariatur facilis porro fugit vero? Perferendis, expedita.</p>',
+  },
+  {
+    id: 'tab-three',
+    title: 'Lorem ipsum dolor,',
+    content:
+      '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi odit ea recusandae expedita tempore voluptas voluptatem consectetur iusto quae, consequuntur minima hic corrupti pariatur facilis porro fugit vero? Perferendis, expedita.</p><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis debitis commodi minima unde, nisi consequuntur reprehenderit. Consequuntur facere quas repellat quo, cumque molestiae nihil minus consequatur cupiditate beatae culpa nobis illo asperiores dolor voluptates iusto quod libero? Incidunt, tempore illo?</p>',
+  },
+];
+
+// click handler
+// const handleClick = id => {
+//   if (current === id) {
+//     current = null;
+//   } else {
+//     current = id;
+//   }
+// }
